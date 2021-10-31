2e137a37a31d89f6110898e8e6d0997fd351b9ea
Mehmet ALBAYRAK <mehmet__albayrak@outlook.com>
2021-10-31 16:49:58 +0300

    day 13 added microservices, AOP

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -10,5 +10,8 @@ namespace Business.Constants
         public static string ProductNameInvalid = "Ürün ismi geçersiz.";
         public static string MaintenanceTime = "Sistem bakım zamanı.";
         public static string ProductsListed = "Ürünler başarılı bir şekilde listelendi.";
+        public static string ProductCountOfCategoryError = "Bir kategoride 10'dan fazla ürün olamaz.";
+        public static string ProductNameAlreadyExists = "Bu isimde zaten başka bir ürün var.";
+        public static string CategoryLimitExceded="Kategori limiti aşıldığı için yeni ürün eklenemiyor.";
     }
 }
